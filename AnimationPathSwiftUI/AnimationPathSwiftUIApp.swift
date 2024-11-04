e75dae37e33422ad27eee11d3161ace465b98098
meowfield <meowfield@yandex.ru>
2024-11-04 19:32:13 +0100

    Initial Commit

---
+++ AnimationPathSwiftUI/AnimationPathSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationPathSwiftUIApp.swift
+//  AnimationPathSwiftUI
+//
+//  Created by Данис Гаязов on 4.11.24..
+//
+
+import SwiftUI
+
+@main
+struct AnimationPathSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationPathSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationPathSwiftUI
+//
+//  Created by Данис Гаязов on 4.11.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
